6840f8918367578302e192dc07573cea131b8d3b
undercontr <onder.alkan15@gmail.com>
2020-10-28 13:07:20 +0300

    Add project files.

---
+++ UnlockPDF/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace UnlockPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PDF seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "pdf";
+            this.openFileDialog1.Filter = "PDF Files|*.pdf";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "PDF dosyaları seçin";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(404, 152);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hiçbir PDF dosyası selçilmedi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(423, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kilidi Aç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(423, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 97);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PDF şifrelerini çözmek ve dosyadan kaldırmak için PDF dosyası seçin ve Kilidi Aç " +
+    "tuşuna basın";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(10, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(485, 23);
+            this.label2.TabIndex = 4;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(13, 206);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 231);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PDF Şifre Kırıcı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ UnlockPDF/Form1.cs
@@ -0,0 +1,118 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnlockPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            textBox1.Text = "";
+
+            textBox1.Text = String.Join(Environment.NewLine, openFileDialog1.SafeFileNames);
+
+            label2.Text = openFileDialog1.FileNames.Length + " dosya seçildi";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.FileNames.Length == 1)
+            {
+
+                try
+                {
+                    var inputFile = openFileDialog1.FileName;
+
+                    PdfDocument maindoc = PdfReader.Open(inputFile, PdfDocumentOpenMode.Import);
+
+                    PdfDocument OutputDoc = new PdfDocument();
+
+                    foreach (PdfPage page in maindoc.Pages)
+                    {
+                        OutputDoc.AddPage(page);
+                    }
+
+                    OutputDoc.Save(inputFile.Replace(".pdf", "_unlocked.pdf"));
+                    maindoc.Dispose();
+                    OutputDoc.Dispose();
+
+                    label2.Text = "Toplamda bir adet dosya şifresi çözüldü. İşlem başarılı.";
+
+                    linkLabel1.Text = "Dosyayı Aç";
+
+                } catch (Exception ex)
+                {
+                    label2.Text = ex.Message;
+                }
+
+            } else if (openFileDialog1.FileNames.Length > 1)
+            {
+                try
+                {
+                    foreach (var file in openFileDialog1.FileNames)
+                    {
+
+
+                        PdfDocument maindoc = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                        PdfDocument OutputDoc = new PdfDocument();
+
+                        foreach (PdfPage page in maindoc.Pages)
+                        {
+                            OutputDoc.AddPage(page);
+                        }
+
+                        OutputDoc.Save(file.Replace(".pdf", "_unlocked.pdf"));
+                        maindoc.Dispose();
+                        OutputDoc.Dispose();
+
+
+                    }
+
+                    label2.Text = "Toplamda " + openFileDialog1.FileNames.Length + " adet dosya şifresi çözüldü. İşlem başarılı.";
+
+                    linkLabel1.Text = "Klasörü Aç";
+                }
+                catch (Exception ex)
+                {
+                    label2.Text = ex.Message;
+                }
+                
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (openFileDialog1.FileNames.Length == 1)
+            {
+                System.Diagnostics.Process.Start(openFileDialog1.FileName.Replace(".pdf", "_unlocked.pdf"));
+            } else if (openFileDialog1.FileNames.Length > 1)
+            {
+
+                var listDir = openFileDialog1.FileNames[0].Split('\\');
+
+                listDir = (string[]) listDir.Take(listDir.Length - 1).ToArray();
+
+                var strListDir = String.Join("\\", listDir);
+
+                System.Diagnostics.Process.Start(strListDir);
+            }
+        }
+    }
+}
